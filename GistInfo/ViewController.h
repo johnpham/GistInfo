9921ffc62cdd4bc87596e8283376699692004a5e
John Pham <JohnPham@Johns-MacBook-Pro.local>
2013-12-16 14:54:13 -0700

    Initial Commit

---
+++ GistInfo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GistInfo
+//
+//  Created by John Pham on 12/16/13.
+//  Copyright (c) 2013 John Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GistInfo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GistInfo
+//
+//  Created by John Pham on 12/16/13.
+//  Copyright (c) 2013 John Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
